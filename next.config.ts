7a0822308f82900d413584b8954c02d026699d5f
bartekmiller <bartek.miller@gmail.com>
2025-01-18 17:40:07 +0100

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'library-zuae.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'library-zuae-lizau98w6-bartekmillers-projects.vercel.app',
+      },
+      {
+        protocol: 'https',
         hostname: 'placehold.co',
       },
       {
@@ -23,6 +31,26 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
 
